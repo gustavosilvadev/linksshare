f70fff74556cfa1f65cbd412d7b67eeb4da52e72
Gustavo <gustavojsilvaweb@gmail.com>
2025-05-05 22:36:36 -0300

    removing console func within main file

--- api/src/main.ts
+++ api/src/main.ts
@@ -7,7 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
-  console.log("PORT >>>>>>>>>>>>>>>> ", process.env.PORT);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
